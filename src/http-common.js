d0c88e7c4b535db3ff9fc8136ae7b7f7f1847c5b
adriano.ament <adrianjoament@gmail.com>
2020-09-06 18:20:44 -0300

    Iniciando deploy

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env);
-
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   baseURL: process.env.REACT_APP_PATH_API,
